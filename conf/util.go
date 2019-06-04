c4a66dd0a694602cdd6e8bf474701a6e3c8dc00e
wzy(Noah Wang) <234082230@qq.com>
2019-06-04 14:19:39 +0800

    [ADD] config util

---
+++ conf/util.go
@@ -0,0 +1,42 @@
+package conf
+
+import (
+	goconfig "github.com/micro/go-config"
+	"log"
+
+	setting_conf "github.com/noahzaozao/go_nwlib/conf/setting"
+)
+
+
+func LoadConfig() *setting_conf.SettingsConfig {
+	// Load json config file
+	if err := goconfig.LoadFile("./config.yaml"); err != nil {
+		log.Println(err.Error())
+		return nil
+	}
+	return GetConfig()
+}
+
+func GetConfig() *setting_conf.SettingsConfig {
+	var settingsConfig setting_conf.SettingsConfig
+	if err := goconfig.Get("config").Scan(&settingsConfig); err != nil {
+		log.Println(err.Error())
+		return nil
+	}
+	return &settingsConfig
+}
+
+func CheckSettingConfig(settingsConfig *setting_conf.SettingsConfig) {
+	log.Println("DEBUG: " + settingsConfig.DEBUG)
+	log.Println("CHARSET: " + settingsConfig.DefaultCharset)
+
+	if len(settingsConfig.DATABASES) < 1 {
+		log.Println("DATABASE config not exist")
+		return
+	}
+
+	if len(settingsConfig.CACHES) < 1 {
+		log.Println("CACHES config not exist")
+		return
+	}
+}
--- go.mod
+++ go.mod
@@ -6,5 +6,11 @@ require (
 	github.com/go-redis/redis v6.15.2+incompatible
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/jinzhu/gorm v1.9.8
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/micro/go-config v1.1.0
 )
+
+replace github.com/sourcegraph/go-diff => github.com/sourcegraph/go-diff v0.5.1
+
+replace github.com/golang/lint v0.0.0-20190313153728-d0100b6bd8b3 => golang.org/x/lint v0.0.0-20190409202823-5614ed5bae6fb75893070bdc0996a68765fdd275
+
+replace github.com/testcontainers/testcontainer-go => github.com/testcontainers/testcontainers-go v0.0.0-20181115231424-8e868ca12c0f
